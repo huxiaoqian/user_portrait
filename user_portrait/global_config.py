195038475d1914c6c1db85aca1555c12ac15da92
linhaobuaa <1257819385@qq.com>
2015-07-04 20:54:28 +0800

    hxq csv2bin 0704

--- user_portrait/global_config.py
+++ user_portrait/global_config.py
@@ -26,8 +26,10 @@ ZMQ_CTRL_VENT_PORT_FLOW2 = '5586'
 ZMQ_VENT_PORT_FLOW3 = '6389'
 ZMQ_CTRL_VENT_PORT_FLOW3 = '5587'
 
+'''
 BIN_FILE_PATH = '/home/ubuntu8/yuankun/data' # '219.224.135.93:/home/ubuntu8/yuankun'
-
+'''
+BIN_FILE_PATH = '/home/ubuntu8/data1309/20130901'
 
 # need three ES identification 
 USER_PROFILE_ES_HOST = ['219.224.135.96','219.224.135.97','219.224.135.98']
