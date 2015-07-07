d6f7e44ca8d1d349aa2430473afbb84fb5c3d4b2
linhaobuaa <1257819385@qq.com>
2015-07-07 20:52:49 +0800

    ljh

--- user_portrait/time_utils.py
+++ user_portrait/time_utils.py
@@ -5,22 +5,22 @@ import time
 def unix2hadoop_date(ts):
     return time.strftime('%Y_%m_%d', time.localtime(ts))
 
-def ts2datetime(ts):
+def ts2date(ts):
     return time.strftime('%Y-%m-%d', time.localtime(ts))
 
-def ts2date(ts):
+def ts2datetime(ts):
     return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(ts))
 
-def datetime2ts(date):
+def date2ts(date):
     return int(time.mktime(time.strptime(date, '%Y-%m-%d')))
 
 def window2time(window, size=24*60*60):
     return window*size
 
-def datetimestr2ts(date):
+def datestr2ts(date):
     return time.mktime(time.strptime(date, '%Y%m%d'))
 
-def ts2datetimestr(ts):
+def ts2datestr(ts):
     return time.strftime('%Y%m%d', time.localtime(ts))
 
 def ts2HourlyTime(ts, interval):
@@ -28,5 +28,3 @@ def ts2HourlyTime(ts, interval):
     ts = ts - ts % interval
     return ts
 
-def ts2datetime_full(ts):
-    return time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(ts))
