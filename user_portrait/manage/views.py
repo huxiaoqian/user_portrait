809f3ca65fdd5eaa0fd017369953168b4f347787
linhaobuaa <1257819385@qq.com>
2015-07-21 09:34:40 +0800

    hxq compare

--- user_portrait/manage/views.py
+++ user_portrait/manage/views.py
@@ -4,20 +4,36 @@ import os
 import time
 import json
 from flask import Blueprint, url_for, render_template, request, abort, flash, session, redirect
-from utils import compare, imagine
+from utils import compare_user_portait, compare_user_activity
+from utils import imagine
 from user_portrait.time_utils import datetime2ts
 
 mod = Blueprint('manage', __name__, url_prefix='/manage')
 
-@mod.route('/compare/')
-def ajax_compare():
+# compare the attr in the es_user_portrait
+@mod.route('/compare_user_portrait/')
+def ajax_compare_user_portrait():
     uid_list = request.args.get('uid_list', '') # uid_list = [uid1, uid2, uid3]
     if uid_list:
-        results = compare(uid_list)
+        results = compare_user_portrait(uid_list)
     if results:
         return json.dumps(results)
     else:
         return None
+
+# compare the detail of activity attribute
+# output data: {user:[{date:weibo_count}, {time_segment:weibo_count}]}
+@mod.route('/compare_user_activity/')
+def ajax_compare_user_activity():
+    uid_list = request.args.get('uid_list', '') # uid_list = [uid1, uid2, uid3]
+    if uid_list:
+        results = compare_user_activity(uid_list)
+    if result:
+        return json.dumps(results)
+    else:
+        return None
+
+
 
 @mod.route('/imagine/')
 def ajax_imagine():
