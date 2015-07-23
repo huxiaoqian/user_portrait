15012d162b8304b2986f91c8380d32464feef3a1
linhaobuaa <1257819385@qq.com>
2015-07-23 14:12:12 +0800

    ywd

--- user_portrait/index/views.py
+++ user_portrait/index/views.py
@@ -15,6 +15,11 @@ def loading():
 
     return render_template('portrait/index.html')
 
+@mod.route('/overview/')
+def overview():
+
+    return render_template('portrait/overview.html')
+
 @mod.route('/contrast/')
 def contrast():
 
