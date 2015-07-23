78931959aaad2f0f745d52d6f9ca98c98fc7c9e4
linhaobuaa <1257819385@qq.com>
2015-07-23 22:14:40 +0800

    yuankun

--- user_portrait/cron/group/utils.py
+++ user_portrait/cron/group/utils.py
@@ -2,12 +2,18 @@
 import sys
 import time
 import json
+from cron_group import get_attr_bci
 reload(sys)
 sys.path.append('../../')
 from time_utils import ts2datetime, datetime2ts
 
+def basic_influence_info(uid_list):
+
+
 def get_attr_influence(uid_list):
     result = {}
+    bci_dict = get_attr_bci(uid_list)
+    
     return result # result = {'influence': count}
 
 
