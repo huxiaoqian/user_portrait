6dddc12fe93474714b86da73ef4b2dec01d68b3d
linhaobuaa <1257819385@qq.com>
2015-07-29 17:33:51 +0800

    zhang

--- user_portrait/static/js/portrait/language.js
+++ user_portrait/static/js/portrait/language.js
@@ -169,8 +169,12 @@ Hashtag.prototype = {   //获取数据，重新画表
 Draw_hashtag:function(data){
 	//console.log(data);
 	var hashtag = document.getElementById('hashtagDes');
-	hashtag.innerHTML = data.hashtag_description;
-	//console.log(data.hashtag_description);
+    if(data.hashtag_description==""){
+        hashtag.innerHTML = '';
+    }else{
+        hashtag.innerHTML = data.hashtag_description;
+    }
+    //console.log(data.hashtag_description);
 	
 }
 }
