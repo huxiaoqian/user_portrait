87f6d3b45c325efdfe6a80674f491b9006f5efc9
linhaobuaa <1257819385@qq.com>
2015-12-27 15:31:53 +0800

    zxy

--- user_portrait/static/js/portrait/influence.js
+++ user_portrait/static/js/portrait/influence.js
@@ -157,10 +157,10 @@ Draw_pie_all0:function(data){
   Draw_user_influence_detail:function(data){
     $('#influence_table').empty();
     var html = '';
-    html += '<table class="table table-striped table-bordered bootstrap-datatable datatable responsive">';
+    html += '<table class="table table-striped table-bordered bootstrap-datatable datatable responsive" style="font-size:14px;">';
     html += '<tr><th rowspan="2" style="text-align:center;vertical-align:middle;">&nbsp;类别</th>';
-    html += '<th id="retweet_distribution" colspan="4" style="text-align:center; font-size:16px;margin-left:10px;cursor: pointer"><u>转发情况</u></th>';
-    html += '<th id="comment_distribution" colspan="4" style="text-align:center; font-size:16px;margin-left:10px;cursor: pointer"><u>评论情况</u></th></tr>';
+    html += '<th colspan="4" style="text-align:center;">转发情况<u id="retweet_distribution" style="font-size:12px;color:#2FA4E7;margin-left:20px;cursor: pointer">查看详情</u></th>';
+    html += '<th colspan="4" style="text-align:center;">评论情况<u id="comment_distribution" style="font-size:12px;color:#2FA4E7;margin-left:20px;cursor: pointer">查看详情</u></th></tr>';
     html += '<tr><th style="text-align:center">转发总数</th><th style="text-align:center">平均数</th><th style="text-align:center">最高数</th><th style="text-align:center">爆发度</th>';
     html += '<th style="text-align:center">评论总数</th><th style="text-align:center">平均数</th><th style="text-align:center">最高数</th><th style="text-align:center">爆发度</th></tr>';
     html += '<tr><th style="text-align:center">原创微博 ('+data['origin_weibo_number']+')</th>';
@@ -291,8 +291,8 @@ function Draw_get_top_weibo(data,div_name){
             //html += "<div style='width:100%;padding:2px;'>";
             // html += "<span style='margin:0 20px'>最高值:<span id='' style='color:red;'>"+data[i].zuigao+"</span></span>";
             // html += "<span style='margin:0 20px;'>爆发度:<span id='' style='color:red;'>"+data[i].baofa+"</span></span>";
-            html += "<span style='float:right;'><span>转发数:<span class='retweet_count' style='font-size:16px;margin-left:10px;cursor: pointer'><u>"+data[i]['1']+"</u></span></span>";
-            html += "<span style='margin-left:10px'>评论数:<span class='comment_count' style='font-size:16px;margin-left:10px;cursor: pointer'><u>"+data[i]['2']+"</u></span></span></span></div>";
+            html += "<span style='float:right;color:#2FA4E7'><span class='retweet_count' style='margin-left:10px;cursor: pointer'><u><b>转发数("+data[i]['1']+")</b></u></span>";
+            html += "<span class='comment_count' style='margin-left:10px;margin-left:10px;cursor: pointer'><u><b>评论数("+data[i]['2']+")</b></u></span></span></div>";
             html += "</div>";
         }
       $('#'+div_name).append(html);
@@ -351,8 +351,7 @@ function click_action(){
 
 }
 
-var weibo3 =['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
-var weibo =[{"text":"【“小学生太需要自由快乐的生活了”】教育专家孙云晓在回忆他的“危险的童年”时说：小学阶段的教育，包括学校、家庭和社会教育的成败得失，将很大程度决定孩子的一生，“小学生太需要自由快乐的生活了，他们上半天课足矣，另外半天适宜参加各种兴趣活动” 。你赞同吗？","zhuanfa":1225453,"pinglun":11425421,"zuigao":16743,"baofa":2135632},{"text":"【“小学生太需要自由快乐的生活了”】教育专家孙云晓在回忆他的“危险的童年”时说：小学阶段的教育，包括学校、家庭和社会教育的成败得失，将很大程度决定孩子的一生，“小学生太需要自由快乐的生活了，他们上半天课足矣，另外半天适宜参加各种兴趣活动” 。你赞同吗？","zhuanfa":123,"pinglun":111,"zuigao":123,"baofa":212},{"text":"【“小学生太需要自由快乐的生活了”】教育专家孙云晓在回忆他的“危险的童年”时说：小学阶段的教育，包括学校、家庭和社会教育的成败得失，将很大程度决定孩子的一生，“小学生太需要自由快乐的生活了，他们上半天课足矣，另外半天适宜参加各种兴趣活动” 。你赞同吗？","zhuanfa":123,"pinglun":111,"zuigao":123,"baofa":212}]
+
 var weibo2 = ['媒体','娱乐','北京','45',['http://tp4.sinaimg.cn/1729736051/50/40018551765/1','http://tp4.sinaimg.cn/1729736051/50/40018551765/1','http://tp4.sinaimg.cn/1729736051/50/40018551765/1','http://tp4.sinaimg.cn/1729736051/50/40018551765/1','http://tp4.sinaimg.cn/1729736051/50/40018551765/1','http://tp4.sinaimg.cn/1729736051/50/40018551765/1','http://tp4.sinaimg.cn/1729736051/50/40018551765/1','http://tp4.sinaimg.cn/1729736051/50/40018551765/1','http://tp4.sinaimg.cn/1729736051/50/40018551765/1'],['http://tp4.sinaimg.cn/1729736051/50/40018551765/1','http://tp4.sinaimg.cn/1729736051/50/40018551765/1','http://tp4.sinaimg.cn/1729736051/50/40018551765/1']]
 var div_name = ['cmt_user','re_user'];
 var Influence = new Influence();
@@ -370,10 +369,6 @@ choose_dayorweek(influence_url);
 
 var basic_influence_url = '/attribute/current_influence_comment/?uid=1220291284&date=2013-09-04';
 Influence.call_sync_ajax_request(basic_influence_url, Influence.ajax_method, Influence.Draw_basic_influence);
-// var yesterday_table_influence_url = '/attribute/current_tag_vector/?uid=1220291284&date=2013-09-01';
-// Influence.call_sync_ajax_request(yesterday_table_influence_url, Influence.ajax_method, Influence.Draw_yesterday_table_influence);
-// var current_influence_comment_url = '/attribute/current_influence_comment/?uid=1220291284&date=2013-09-01';
-// Influence.call_sync_ajax_request(current_influence_comment_url, Influence.ajax_method, Influence.Draw_current_influence_comment);
 
 var user_influence_detail_url = '/attribute/user_influence_detail/?uid=1197161814&date=2013-09-01';
 Influence.call_sync_ajax_request(user_influence_detail_url, Influence.ajax_method, Influence.Draw_user_influence_detail);
@@ -381,7 +376,6 @@ Influence.call_sync_ajax_request(user_influence_detail_url, Influence.ajax_metho
 
 var all_influenced_users_url_style0 = '/attribute/all_influenced_users/?uid=1218353337&date=2013-09-02&style=0';
 Influence.call_sync_ajax_request(all_influenced_users_url_style0, Influence.ajax_method, Influence.Draw_all_influenced_users_style0);
-
 var all_influenced_users_url_style1 = '/attribute/all_influenced_users/?uid=1218353337&date=2013-09-02&style=1';
 Influence.call_sync_ajax_request(all_influenced_users_url_style1, Influence.ajax_method, Influence.Draw_all_influenced_users_style1);
 
@@ -394,11 +388,8 @@ var get_top_weibo_url_style2 = '/attribute/get_top_weibo/?uid=1182391231&date=20
 Influence.call_sync_ajax_request(get_top_weibo_url_style2, Influence.ajax_method, Influence.Draw_get_top_weibo3);
 var get_top_weibo_url_style3 = '/attribute/get_top_weibo/?uid=1182391231&date=2013-09-05&style=3';
 Influence.call_sync_ajax_request(get_top_weibo_url_style3, Influence.ajax_method, Influence.Draw_get_top_weibo4);
-
 var influence_tag_url = '/attribute/current_tag_vector/?uid=1218353337&date=2013-09-02';
 Influence.call_sync_ajax_request(influence_tag_url, Influence.ajax_method, Influence.Influence_tag_vector);
-
-
 click_action();
 
       
