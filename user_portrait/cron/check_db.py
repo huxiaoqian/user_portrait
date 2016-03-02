27b2407c40f9e5cb15d89a9995e0a2cac0783f50
linhaobuaa <1257819385@qq.com>
2016-03-02 10:09:28 +0800

    check_db.py

--- user_portrait/cron/check_db.py
+++ user_portrait/cron/check_db.py
@@ -1,38 +1,42 @@
 # -*- coding:utf-8 -*-
 
+"""
+本程序用于实时监测redis和elasticsearch的状态，如果有数据库挂掉，应当在下个时间段内重启
+
+"""
 import subprocess
 import sys
 import os
 import time
-
+reload(sys)
+sys.path.append('./../../')
+from global_config import redis_path, es_path
 
 def check_redis(p_name):
     cmd = 'ps -ef|grep %s|grep -v "grep"' % p_name
-    restart_cmd = 'cd /home/ubuntu3/huxiaoqian/redis-2.8.13 && src/redis-server redis.conf'
+    restart_cmd = 'cd %s && src/redis-server redis.conf' % redis_path
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     (stdoutput, erroutput) = p.communicate()
     val = stdoutput
     if p_name in val:
-        print "ok - %s process is running" % p_name
+        print "%s %s running" % (time.ctime(),p_name)
     else:
         os.system(restart_cmd)
+        print "%s %s restart" % (time.ctime(), p_name)
 
 def check_elasticsearch(p_name):
     cmd = 'ps -ef|grep %s|grep -v "grep"' % p_name
-    restart_cmd = 'cd /home/ubuntu3/yuankun/elasticsearch-1.6.0 && bin/elasticsearch -Xmx25g -Xms25g -Des.max-open-files=true -d'
+    restart_cmd = 'cd %s && bin/elasticsearch -Xmx25g -Xms25g -Des.max-open-files=true -d' % es_path
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     (stdoutput, erroutput) = p.communicate()
     if p_name in stdoutput:
-        print "%s ok - %s process is running" % (time.ctime(), p_name)
+        print "%s %s running" % (time.ctime(), p_name)
     else:
         os.system(restart_cmd)
+        print "%s %s restart" % (time.ctime(), p_name)
 
 if __name__ == '__main__':
 
-    # test procedure running
-    # 8-24: zmq_vent_weibo.py
-    # 8-4: zmq_work_weibo.py
-    # 4-8: redis_to_es.py
 
     # test redis running
     check_redis("redis")
